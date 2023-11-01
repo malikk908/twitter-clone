c8c76aa3eb4b1fda76e7320a80f7ed70ffe6f65d
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-11-01 15:21:39 +0500

    eslint update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,8 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/prefer-nullish-coalescing" : "off",
+    "@typescript-eslint/prefer-optional-chain" : "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
